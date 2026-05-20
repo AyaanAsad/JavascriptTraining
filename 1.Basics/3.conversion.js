c72219b621f6c09cab27be1811f16d1e87762528
Ayaan Asad <ayaan.asad.2104@gmail.com>
2026-05-20 15:07:09 +0000

    Operators added

--- 1.Basics/3.conversion.js
+++ 1.Basics/3.conversion.js
@@ -13,7 +13,9 @@ console.log(typeof ValueInNumber)
 33abc -> NaN (type = Number) Not a Number
 true -> 1 ; false -> 0
 */
+
 //--------------------------------------------BOOLEAN---------------------------------------------
+
 let isLoggedIn = 1;
 
 let isLoggedInBool = Boolean(isLoggedIn)
@@ -23,3 +25,26 @@ console.log(isLoggedInBool);
 1 -> true; 0 -> false
 Non empty string -> true ; Empty string -> false;
 */
+
+//----------------------------------------------OPERATIONS-----------------------------------------
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + (2 + 2)) // 14
+console.log("1" + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+console.log(+true) //1
+console.log(+"") //0
+
+let number = 10
+console.log(number++) // 10
+console.log(++number) // 12
