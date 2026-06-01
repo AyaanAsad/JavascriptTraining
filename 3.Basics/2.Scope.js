07d5d6e6e3d6eadfaa871de5e51b40fdc4486ea8
Ayaan Asad <ayaan.asad.2104@gmail.com>
2026-06-01 05:27:41 +0000

    Scope of nested Functions

--- 3.Basics/2.Scope.js
+++ 3.Basics/2.Scope.js
@@ -12,3 +12,39 @@ console.log("Outer let: ", a)
 console.log("Outer var: ",c)
 
 //Do not use var because golbal scope and block scope for var is shared (or same)
+
+//-----------------------------------NESTED FUNCTIONS--------------------------------------------
+
+function parent(){
+    const name = "Lorem"
+    function child(){
+        const school  = "Ipsum"
+        console.log(name) // Will print as block scope of parent() is golbal scope of child()
+    }
+    //console.log(school) <- Wont print as school is in block scope of child(), parent cannnot access child variables, vice versa is true
+    child()
+}
+parent()
+
+
+if (true) {
+    const username = "Lorem"
+    if (username === "Lorem") {
+        const website = " Ipsum"
+        console.log(username + website);
+    }
+    // console.log(website); <- Error for obvious reasons
+}
+// console.log(username); <- Same here
+
+//-----------------------------RANDOM MINI HOISTING EXAMPLE------------------------------------
+
+console.log(addone(5)) // Will work as the function is not store in a variable and is declared independently 
+function addone(num){
+    return num + 1
+}
+
+addTwo(5) // Will not work as function is in a variable which is defined after the this line
+const addTwo = function(num){
+    return num + 2
+}
