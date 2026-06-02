27e59069237683758d5fdbaa13c43934bf9b8be4
Ayaan Asad <ayaan.asad.2104@gmail.com>
2026-06-02 07:53:37 +0000

    Filters

---
+++ 5.Iterations/6.Filter.js
@@ -0,0 +1,59 @@
+// const number = [10,11,12,13,14,15]
+
+// const values = number.forEach( (element) => {
+//     console.log(element-1)
+//     return element
+// })
+//For Each does not return a value
+
+// console.log(values)
+
+//WHICH IS WHY WE DO THIS :
+
+const Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//IMPLICIT RETURN:
+const newnums1 = Numbers.filter((num) => num > 4)
+// console.log (newnums1)
+
+//EXPLICIT RETURN:
+const newnums = Numbers.filter((num) => {
+    if (num > 4) {
+        return num // not console.log as we are in arrow function's block scope
+    }
+})
+// console.log (newnums)
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+const userbooks1 = books.filter((item) => {
+    if (item.genre === "History") {
+        return item
+    }
+})
+
+const userbooks2 = books.filter((item) => {
+    if (item.publish > 2000) {
+        return item
+    }
+})
+
+const userbooks3 = books.filter((item) => {
+    if (item.publish >= 1995 && item.genre === "History") {
+        return item
+    }
+})
+
+
+console.log(userbooks3)
